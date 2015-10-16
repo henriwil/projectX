24d7640c2c87d3606c1965b1329f3bb72f56447b
henriwil <henrik@dhcp-10-20-73-139.wlan.ntnu.no>
2015-10-16 12:19:57 +0200

    vegger

---
+++ Assets/living_wall.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+var scale:float;
+var varighet:float;
+var Timer=0.0;
+var intensitet:double;
+
+function Start () {
+
+}
+
+function Update () 
+{
+	while(transform.localScale.y>0)
+	{
+		transform.localScale.y -= Timer;
+		Debug.Log(Timer);
+	}	
+	Timer=0.0;
+	
+	while(transform.localScale.y<1)
+	{
+		
+	}	
+	Timer=0.0;
+}
---
+++ Assets/pulse.js
@@ -0,0 +1,29 @@
+﻿#pragma strict
+var scale:float=1;
+var varighet:float=1.5;
+var direction:float=1; //1= y, 2=x
+var Timer=0.0;
+var intensitet:double;
+
+function Start () {
+
+}
+
+function Update () 
+{
+	while(Timer<5)
+	{
+		transform.localScale.y-=Timer;
+		Timer+=1;
+		Debug.Log(Timer+"midnre");
+	}	
+	Timer=0.0;
+	
+	while(Timer<5)
+	{
+		transform.localScale.y+=Timer;
+		Timer+=1;
+		Debug.Log(Timer+"storre");
+	}	
+	Timer=0.0;
+}
